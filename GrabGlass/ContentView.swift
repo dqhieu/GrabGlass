2096a759f1b6b6369a6bd5a6b52b9c89043c6684
dqhieu <dqhieu13@gmail.com>
2025-06-18 09:10:45 +0800

    Initial commit

---
+++ GrabGlass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GrabGlass
+//
+//  Created by Dinh Quang Hieu on 18/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ GrabGlass/GrabGlassApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrabGlassApp.swift
+//  GrabGlass
+//
+//  Created by Dinh Quang Hieu on 18/6/25.
+//
+
+import SwiftUI
+
+@main
+struct GrabGlassApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
